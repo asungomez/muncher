1269533dd8c2a8a0bf5792a90ccc9d625e603203
Asun Gomez Castro <agcastro7@gmail.com>
2025-08-26 11:44:53 +0200

    fix cursor errors

--- memoria/make-memoria.sh
+++ memoria/make-memoria.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p generated
+rm -f src/index.aux src/index.fdb_latexmk src/index.fls src/index.log src/index.out src/index.toc src/index.pdf
 pdflatex -output-directory=generated -jobname=memoria src/index.tex
+rm -f src/index.aux src/index.fdb_latexmk src/index.fls src/index.log src/index.out src/index.toc src/index.pdf src/index.synctex.gz
