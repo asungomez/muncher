940f0adc7dda7f40140342325ffa092823a3e636
Asun <agcastro7@gmail.com>
2026-03-26 12:07:47 +0100

    add spell checking to ESLint

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import cspellPlugin from "@cspell/eslint-plugin";
+import * as jsonParser from "jsonc-eslint-parser";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
@@ -23,6 +25,27 @@ export default tseslint.config([
       "no-debugger": "error",
       "no-eval": "error",
       "no-console": "error",
+    },
+  },
+  {
+    files: ["src/i18n/locales/*.json"],
+    languageOptions: {
+      parser: jsonParser, // Use the imported parser object
+    },
+    plugins: {
+      "@cspell": cspellPlugin, // Define as an object, not a string array
+    },
+    rules: {
+      "@cspell/spellchecker": [
+        "error",
+        {
+          // Adjusting scopes to ONLY check keys
+          checkScope: [
+            ["JSONProperty[key] JSONLiteral", false], // Ignore keys
+            ["JSONProperty[value] JSONLiteral", true], // Target values
+          ],
+        },
+      ],
     },
   },
 ]);
